b670287a4fcc2bfb1d43a0fbe0a0e825f060ba91
Sayeb Mohaimen <muhaimen360@gmail.com>
2024-07-06 18:39:02 +0600

    User Image

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from 'react';
 import {useLocation} from 'react-router-dom';
 import DashSidebar  from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-export default function Dashboard() {
+export default function Dashboard() { 
+  const location=useLocation() 
+  const [tab, setTab]=useState(' ') 
+  useEffect(()=>{ 
+    const urlParams=new URLSearchParams(location.search) 
+    const tabFromUrl=urlParams.get('tab') 
+    if(tabFromUrl){ 
+      setTab(tabFromUrl);
+    }
+  },[location.search])
   
   return (
-    <div>
-      <div className="">
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className='md:w-56'>
         <DashSidebar/>
 
       </div>
